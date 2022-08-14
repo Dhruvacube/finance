60129cedd5a6943ac03205e2ebc651baac0b1da2
Miku Chan <34207400+Dhruvacube@users.noreply.github.com>
2022-08-14 08:45:37 +0530

    some requirements and logging changes

--- finance/django_logging.py
+++ finance/django_logging.py
@@ -42,7 +42,7 @@ LOGGING = {
     },
     "loggers": {
         "django": {
-            "level": "INFO",
+            "level": "DEBUG",
             "handlers": [
                 "console",
             ],
